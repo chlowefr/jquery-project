230f90a14305dcd56dbd34d8b9de9d7189134887
Cloclo_du_33 <chlowe_fr@yahoo.com>
2019-02-07 14:59:40 +0100

    Add arrow to top

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+$(window).on("scroll",function(){
+    let positionY = $(this).scrollTop();
+    console.log(positionY);
+
+    //Afficher arrtotop une fois en bas de la page.
+    if(positionY > 1700){
+        $("#scrolltotop").css("opacity", 1);
+    }
+    else if(positionY < 1700){
+        $("#scrolltotop").css("opacity", 0);
+    }
+
+})
