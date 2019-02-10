4c960434dcf9bcf4ffa318b7e4ee88ff208ca702
Cloclo_du_33 <chlowe_fr@yahoo.com>
2019-02-10 13:49:47 +0100

    Modif arrow

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ $(window).on("scroll",function(){
     console.log(positionY);
 
     //Afficher arrtotop une fois en bas de la page.
-    if(positionY > 1700){
+    if(positionY > 1500){
         $("#scrolltotop").css("opacity", 1);
     }
-    else if(positionY < 1700){
+    else if(positionY < 1500){
         $("#scrolltotop").css("opacity", 0);
+    }
+
+    //Lorsque positionY est supérieure à 100
+    else if(positionY > 100){
+        //réduire height de la nav bar
+        $("nav").css("height", "40px")
+        //
+        
     }
 
 })
