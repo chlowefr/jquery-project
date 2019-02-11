94f866ba772005be09553df4334a51c7843c31f1
Cloclo_du_33 <chlowe_fr@yahoo.com>
2019-02-11 21:00:43 +0100

    Modif reduc nav bar

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-$(window).on("scroll",function(){
+$(window).on("scroll", function () {
     let positionY = $(this).scrollTop();
     console.log(positionY);
 
     //Afficher arrtotop une fois en bas de la page.
-    if(positionY > 1500){
+    if (positionY > 1500) {
         $("#scrolltotop").css("opacity", 1);
     }
-    else if(positionY < 1500){
+    else if (positionY < 1500) {
         $("#scrolltotop").css("opacity", 0);
     }
 
+})
+$(window).on("scroll", function () {
+    let positionNav = $(this).scrollTop();
+    console.log(positionNav);
+
     //Lorsque positionY est supérieure à 100
-    else if(positionY > 100){
+    if (positionNav > 100) {
         //réduire height de la nav bar
-        $("nav").css("height", "40px")
-        //
-        
+        $(".nav-bar").css("height", "50px")
+    }
+    else if (positionNav < 100) {
+        //réduire height de la nav bar
+        $(".nav-bar").css("height", "75px");
     }
 
 })
